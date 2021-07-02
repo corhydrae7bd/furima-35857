94c5a5f139f758c0a70194f11ccaf1a9757a5840
corhydrae7bd <corhydrae7bd@gmail.com>
2021-07-02 15:50:12 +0900

    itemのバリデージョン設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,29 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+
+  with_options presence: true do
+    validates :image
+    validates :items_name
+    validates :items_description
+    validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters", allow_blank: true}
+  end
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range", allow_blank: true}
+
+  with_options numericality: { other_than: 1, message:"can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :burden_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+  end
+
 end
