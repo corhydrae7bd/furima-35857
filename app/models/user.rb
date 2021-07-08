f71204f9550cb0863378bd3a7dcaaa5fd375ce49
corhydrae7bd <corhydrae7bd@gmail.com>
2021-07-09 03:56:19 +0900

    Revert "recordとaddress関係のAssociationの設定"
    This reverts commit 7794e3e7f08ab23cb962e6f3d527cbecfa75447c.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_many :records
   
   with_options presence: true do
     validates :nickname
